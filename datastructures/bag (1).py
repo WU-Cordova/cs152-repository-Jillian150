fc863021b2a0d9e0771134ad8244148a186cc3ac
Jillian150 <jsprice@willamette.edu>
2025-01-28 20:25:18 -0800

    Completed Bag Implementation

--- datastructures/bag (1).py
+++ datastructures/bag (1).py
@@ -4,27 +4,31 @@ from collections import Counter
 
 class Bag(IBag[T]):
     def __init__(self, *items: Optional[Iterable[T]]) -> None:
-        self.bag_dict: dict = True
+        self.bag_dict = Counter()
+        for item in items:
+            if item is not None:
+                self.bag_dict.update(item)
 
     def add(self, item: T) -> None:
-        self.add(item)
-        Counter(self.bag_dict + 1)
+        self.bag_dict[item] += 1
 
     def remove(self, item: T) -> None:
-        self.bag_dict.pop(item)
-        Counter(self.bag_dict - 1)
+        if self.bag_dict[item] > 0:
+            self.bag_dict[item] -= 1
+        if self.bag_dict[item] == 0:
+            del self.bag_dict[item]
 
     def count(self, item: T) -> int:
-        raise NotImplementedError("count method not implemented")
+        return self.bag_dict.get(item, 0)
 
     def __len__(self) -> int:
-        raise NotImplementedError("__len__ method not implemented")
+        return sum(self.bag_dict.values())
 
     def distinct_items(self) -> Iterable[T]:
-        raise NotImplementedError("distinct_items method not implemented")
+        return self.bag_dict.keys()
 
     def __contains__(self, item) -> bool:
-        raise NotImplementedError("__contains__ method not implemented")
+        return item in self.bag_dict
 
     def clear(self) -> None:
-        raise NotImplementedError("clear method not implemented")
+        self.bag_dict.clear()
