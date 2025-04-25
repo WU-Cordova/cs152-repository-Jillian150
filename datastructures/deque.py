b98db2418914104cf9dea5b395db6d32fbfc59fc
Jillian150 <jsprice@willamette.edu>
2025-04-25 15:36:08 -0700

    Edited Linked list from comments from code grade

--- datastructures/deque.py
+++ datastructures/deque.py
@@ -1,163 +1,120 @@
-import os
-from datastructures.iqueue import IQueue
-from datastructures.linkedlist import LinkedList
-from typing import TypeVar
+from __future__ import annotations
+from typing import Optional, TypeVar, Generic, Sequence
 
 T = TypeVar('T')
 
-class Deque[T](IQueue[T]):
-    """
-    A double-ended queue (deque) implementation.
-    """
+class LinkedList(Generic[T]):
+    class Node:
+        def __init__(self, data: T, next: Optional[LinkedList.Node] = None, previous: Optional[LinkedList.Node] = None) -> None:
+            self.data = data
+            self.next = next
+            self.previous = previous
 
-    def __init__(self, data_type: type = object) -> None:
-        """
-        Initializes the deque with a specified data type.
+    def __init__(self) -> None:
+        self.head: Optional[LinkedList.Node] = None
+        self.tail: Optional[LinkedList.Node] = None
+        self.count = 0
 
-        Args:
-            - data_type (type): The type of data the deque will hold.
-        """
-        raise NotImplementedError("Deque initialization is not implemented.")
+    def append(self, item: T) -> None:
+        """Adds an item to the end of the list."""
+        new_node = self.Node(item)
+        if self.tail:
+            self.tail.next = new_node
+            new_node.previous = self.tail
+        else:
+            self.head = new_node
+        self.tail = new_node
+        self.count += 1
 
-    def enqueue(self, item: T) -> None:
-        """
-        Adds an item to the back of the deque.
+    def prepend(self, item: T) -> None:
+        """Adds an item to the beginning of the list."""
+        new_node = self.Node(item)
+        if self.head:
+            self.head.previous = new_node
+            new_node.next = self.head
+        else:
+            self.tail = new_node
+        self.head = new_node
+        self.count += 1
 
-        Args:
-            - item (T): The item to add to the back of the deque.
+    def pop(self) -> T:
+        """Removes and returns the last item in the list."""
+        if not self.tail:
+            raise IndexError("List is empty")
+        value = self.tail.data
+        self.tail = self.tail.previous
+        if self.tail:
+            self.tail.next = None
+        else:
+            self.head = None
+        self.count -= 1
+        return value
 
-        Raises:
-            - TypeError: If the item is not of the correct type.
-        """
-        raise NotImplementedError("Method to add an item to the back of the deque is not implemented.")
+    def pop_front(self) -> T:
+        """Removes and returns the first item in the list."""
+        if not self.head:
+            raise IndexError("List is empty")
+        value = self.head.data
+        self.head = self.head.next
+        if self.head:
+            self.head.previous = None
+        else:
+            self.tail = None
+        self.count -= 1
+        return value
 
-    def dequeue(self) -> T:
-        """
-        Removes and returns the item from the front of the deque.
-
-        Returns:
-            - T: The item removed from the front of the deque.
-
-        Raises:
-            - IndexError: If the deque is empty.
-        """
-        raise NotImplementedError("Method to remove an item from the front of the deque is not implemented.")
-
-    def enqueue_front(self, item: T) -> None:
-        """
-        Adds an item to the front of the deque.
-
-        Args:
-            - item (T): The item to add to the front of the deque.
-
-        Raises:
-            - TypeError: If the item is not of the correct type.
-        """
-        raise NotImplementedError("Method to add an item to the front of the deque is not implemented.")
-
-    def dequeue_back(self) -> T:
-        """
-        Removes and returns the item from the back of the deque.
-
-        Returns:
-            - T: The item removed from the back of the deque.
-
-        Raises:
-            - IndexError: If the deque is empty.
-        """
-        raise NotImplementedError("Method to remove an item from the back of the deque is not implemented.")
-
+    @property
     def front(self) -> T:
-        """
-        Returns the front item of the deque without removing it.
+        """Returns the first item in the list."""
+        if not self.head:
+            raise IndexError("List is empty")
+        return self.head.data
 
-        Returns:
-            - T: The front item of the deque.
-
-        Raises:
-            - IndexError: If the deque is empty.
-        """
-        raise NotImplementedError("Method to get the front item of the deque is not implemented.")
-
+    @property
     def back(self) -> T:
-        """
-        Returns the back item of the deque without removing it.
-
-        Returns:
-            - T: The back item of the deque.
-
-        Raises:
-            - IndexError: If the deque is empty.
-        """
-        raise NotImplementedError("Method to get the back item of the deque is not implemented.")
-
-    def empty(self) -> bool:
-        """
-        Checks if the deque is empty.
-
-        Returns:
-            - bool: True if the deque is empty, False otherwise.
-        """
-        raise NotImplementedError("Method to check if the deque is empty is not implemented.")
+        """Returns the last item in the list."""
+        if not self.tail:
+            raise IndexError("List is empty")
+        return self.tail.data
 
     def __len__(self) -> int:
-        """
-        Returns the number of items in the deque.
+        """Returns the number of items in the list."""
+        return self.count
 
-        Returns:
-            - int: The number of items in the deque.
-        """
-        raise NotImplementedError("Method to get the length of the deque is not implemented.")
-    
     def __contains__(self, item: T) -> bool:
-        """
-        Checks if an item exists in the deque.
+        """Checks if an item exists in the list."""
+        current = self.head
+        while current:
+            if current.data == item:
+                return True
+            current = current.next
+        return False
 
-        Args:
-            - item (T): The item to check for existence.
+    def clear(self) -> None:
+        """Clears all items from the list."""
+        self.head = None
+        self.tail = None
+        self.count = 0
 
-        Returns:
-            - bool: True if the item exists in the deque, False otherwise.
-        """
-        raise NotImplementedError("Method to check if an item exists in the deque is not implemented.")
-    
-    def __eq__(self, other) -> bool:
-        """
-        Compares two deques for equality.
+    def __iter__(self):
+        """Returns an iterator over the list."""
+        self._current = self.head
+        return self
 
-        Args:
-            - other (Deque): The deque to compare with.
-
-        Returns:
-            - bool: True if the deques are equal, False otherwise.
-        """
-        raise NotImplementedError("Method to compare two deques is not implemented.")
-    
-    def clear(self):
-        """
-        Clears all items from the deque.
-        """
-        raise NotImplementedError("Method to clear the deque is not implemented.")
+    def __next__(self) -> T:
+        """Returns the next item in the iteration."""
+        if self._current:
+            data = self._current.data
+            self._current = self._current.next
+            return data
+        else:
+            raise StopIteration
 
     def __str__(self) -> str:
-        """
-        Returns a string representation of the deque.
-
-        Returns:
-            - str: A string representation of the deque.
-        """
-        raise NotImplementedError("Method to get the string representation of the deque is not implemented.")
-    
-    def __repr__(self) -> str:
-        """
-        Returns a detailed string representation of the deque.
-
-        Returns:
-            - str: A detailed string representation of the deque.
-        """
-        raise NotImplementedError("Method to get the detailed string representation of the deque is not implemented.")
-
-
-if __name__ == '__main__':
-    filename = os.path.basename(__file__)
-    print(f'OOPS!\nThis is the {filename} file.\nDid you mean to run your tests or program.py file?\nFor tests, run them from the Test Explorer on the left.')
+        """Returns a string representation of the list."""
+        values = []
+        current = self.head
+        while current:
+            values.append(repr(current.data))
+            current = current.next
+        return '[' + ' <-> '.join(values) + ']'
--- datastructures/liststack.py
+++ datastructures/liststack.py
@@ -1,133 +1,200 @@
-import os
-from datastructures.istack import IStack
-from typing import Generic
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import Optional, Sequence, Iterator
+from datastructures.ilinkedlist import ILinkedList, T
 
-from datastructures.linkedlist import LinkedList
 
-class ListStack[T](Generic[T], IStack[T]):
-    """
-    ListStack (LinkedList-based Stack)
+class LinkedList[T](ILinkedList[T]):
 
-    """
+    @dataclass
+    class Node:
+        data: T
+        next: Optional[LinkedList.Node] = None
+        previous: Optional[LinkedList.Node] = None
 
-    def __init__(self, data_type:object) -> None:
-        """
-        Initializes the ListStack.
+    def __init__(self, data_type: type = object) -> None:
+        self.head: Optional[LinkedList.Node] = None
+        self.tail: Optional[LinkedList.Node] = None
+        self.count: int = 0
+        self.data_type = data_type
+        self.current: Optional[LinkedList.Node] = None  # Iterator tracking
 
-        Args:
-            data_type (type): The type of data the stack will hold.
+    @staticmethod
+    def from_sequence(sequence: Sequence[T], data_type: type = object) -> LinkedList[T]:
+        ll = LinkedList(data_type)
+        for item in sequence:
+            ll.append(item)
+        return ll
 
-        """
-        raise NotImplementedError("ListStack.__init__ is not implemented.")
+    def append(self, item: T) -> None:
+        self._check_type(item)
+        new_node = LinkedList.Node(item)
+        if self.tail:
+            self.tail.next = new_node
+            new_node.previous = self.tail
+        else:
+            self.head = new_node
+        self.tail = new_node
+        self.count += 1
 
-    def push(self, item: T):
-        """
-        Pushes an item onto the stack.
+    def prepend(self, item: T) -> None:
+        self._check_type(item)
+        new_node = LinkedList.Node(item)
+        if self.head:
+            self.head.previous = new_node
+            new_node.next = self.head
+        else:
+            self.tail = new_node
+        self.head = new_node
+        self.count += 1
 
-        Args:
-            item (T): The item to push onto the stack.
-        
-        Raises:
-            TypeError: If the item is not of the correct type.
+    def insert_before(self, target: T, item: T) -> None:
+        self._check_type(item)
+        current = self.head
+        while current:
+            if current.data == target:
+                new_node = LinkedList.Node(item, next=current, previous=current.previous)
+                if current.previous:
+                    current.previous.next = new_node
+                else:
+                    self.head = new_node
+                current.previous = new_node
+                self.count += 1
+                return
+            current = current.next
+        raise ValueError(f"Target {target} not found in the list.")
 
-        """
-        raise NotImplementedError("ListStack.push is not implemented.")
+    def insert_after(self, target: T, item: T) -> None:
+        self._check_type(item)
+        current = self.head
+        while current:
+            if current.data == target:
+                new_node = LinkedList.Node(item, previous=current, next=current.next)
+                if current.next:
+                    current.next.previous = new_node
+                else:
+                    self.tail = new_node
+                current.next = new_node
+                self.count += 1
+                return
+            current = current.next
+        raise ValueError(f"Target {target} not found in the list.")
+
+    def remove(self, item: T) -> None:
+        current = self.head
+        while current:
+            if current.data == item:
+                if current.previous:
+                    current.previous.next = current.next
+                else:
+                    self.head = current.next
+                if current.next:
+                    current.next.previous = current.previous
+                else:
+                    self.tail = current.previous
+                self.count -= 1
+                return
+            current = current.next
+        raise ValueError(f"Item {item} not found in the list.")
+
+    def remove_all(self, item: T) -> None:
+        current = self.head
+        while current:
+            if current.data == item:
+                if current.previous:
+                    current.previous.next = current.next
+                else:
+                    self.head = current.next
+                if current.next:
+                    current.next.previous = current.previous
+                else:
+                    self.tail = current.previous
+                self.count -= 1
+            current = current.next
 
     def pop(self) -> T:
-        """
-        Removes and returns the top item from the stack.
+        if not self.tail:
+            raise IndexError("Pop from empty list")
+        value = self.tail.data
+        if self.tail.previous:
+            self.tail = self.tail.previous
+            self.tail.next = None
+        else:
+            self.head = self.tail = None
+        self.count -= 1
+        return value
 
-        Returns:
-            T: The top item from the stack.
-        
-        Raises:
-            IndexError: If the stack is empty.
-        """
-        raise NotImplementedError("ListStack.pop is not implemented.")
+    def pop_front(self) -> T:
+        if not self.head:
+            raise IndexError("Pop from empty list")
+        value = self.head.data
+        if self.head.next:
+            self.head = self.head.next
+            self.head.previous = None
+        else:
+            self.head = self.tail = None
+        self.count -= 1
+        return value
 
-    def peek(self) -> T:
-        """
-        Returns the top item from the stack without removing it.
+    @property
+    def front(self) -> T:
+        if not self.head:
+            raise IndexError("Front from empty list")
+        return self.head.data
 
-        Returns:
-            T: The top item from the stack.
-        
-        Raises:
-            IndexError: If the stack is empty.
-        """
-        raise NotImplementedError("ListStack.peek is not implemented.")
+    @property
+    def back(self) -> T:
+        if not self.tail:
+            raise IndexError("Back from empty list")
+        return self.tail.data
 
     @property
     def empty(self) -> bool:
-        """
-        Checks if the stack is empty.
-
-        Returns:
-            bool: True if the stack is empty, False otherwise.
-        """
-        raise NotImplementedError("ListStack.empty is not implemented.")
-
-    def clear(self):
-        """
-        Clears all items from the stack.
-        """
-        raise NotImplementedError("ListStack.clear is not implemented.")
-
-    def __contains__(self, item: T) -> bool:
-        """
-        Checks if an item exists in the stack.
-
-        Args:
-            item (T): The item to check for.
-
-        Returns:
-            bool: True if the item exists in the stack, False otherwise.
-
-        """
-        raise NotImplementedError("ListStack.__contains__ is not implemented.")
-
-    def __eq__(self, other) -> bool:
-        """
-        Compares two stacks for equality.
-
-        Args:
-            other (ListStack): The stack to compare with.
-
-        Returns:
-            bool: True if the stacks are equal, False otherwise.
-
-        """
-        raise NotImplementedError("ListStack.__eq__ is not implemented.")
+        return self.count == 0
 
     def __len__(self) -> int:
-        """
-        Returns the number of items in the stack.
+        return self.count
 
-        Returns:
-            int: The number of items in the stack.
-        """
-        raise NotImplementedError("ListStack.__len__ is not implemented.")
+    def clear(self) -> None:
+        self.head = self.tail = None
+        self.count = 0
+
+    def __contains__(self, item: T) -> bool:
+        current = self.head
+        while current:
+            if current.data == item:
+                return True
+            current = current.next
+        return False
+
+    def __iter__(self) -> Iterator[T]:
+        self.current = self.head
+        return self
+
+    def __next__(self) -> T:
+        if not self.current:
+            raise StopIteration
+        value = self.current.data
+        self.current = self.current.next
+        return value
+
+    def __reversed__(self) -> Iterator[T]:
+        current = self.tail
+        while current:
+            yield current.data
+            current = current.previous
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, LinkedList):
+            return False
+        return list(self) == list(other)
 
     def __str__(self) -> str:
-        """
-        Returns a string representation of the stack.
-
-        Returns:
-            str: A string representation of the stack.
-        """
-        raise NotImplementedError("ListStack.__str__ is not implemented.")
+        return f"[{', '.join(map(str, self))}]"
 
     def __repr__(self) -> str:
-        """
-        Returns a detailed string representation of the stack.
+        return f"LinkedList({', '.join(map(str, self))}) Count: {self.count}"
 
-        Returns:
-            str: A detailed string representation of the stack.
-
-        """
-        raise NotImplementedError("ListStack.__repr__ is not implemented.")
-    
-
-if __name__ == '__main__':
-    filename = os.path.basename(__file__)
-    print(f'OOPS!\nThis is the {filename} file.\nDid you mean to run your tests or program.py file?\nFor tests, run them from the Test Explorer on the left.')
+    def _check_type(self, item: T) -> None:
+        if not isinstance(item, self.data_type):
+            raise TypeError(f"Item {item} must be of type {self.data_type}")
