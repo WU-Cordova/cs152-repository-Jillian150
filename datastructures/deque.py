389396480d38e27b5ae32c9917895cd8ded3eff0
Jillian150 <jsprice@willamette.edu>
2025-04-25 15:51:38 -0700

    Edited deque.py

--- datastructures/deque.py
+++ datastructures/deque.py
@@ -1,120 +1,58 @@
-from __future__ import annotations
-from typing import Optional, TypeVar, Generic, Sequence
+from typing import TypeVar, Generic
 
-T = TypeVar('T')
+T = TypeVar("T")
 
-class LinkedList(Generic[T]):
-    class Node:
-        def __init__(self, data: T, next: Optional[LinkedList.Node] = None, previous: Optional[LinkedList.Node] = None) -> None:
-            self.data = data
-            self.next = next
-            self.previous = previous
-
+class Deque(Generic[T]):
     def __init__(self) -> None:
-        self.head: Optional[LinkedList.Node] = None
-        self.tail: Optional[LinkedList.Node] = None
-        self.count = 0
+        """Initializes an empty deque using LinkedList."""
+        self._list = LinkedList[T]()
 
     def append(self, item: T) -> None:
-        """Adds an item to the end of the list."""
-        new_node = self.Node(item)
-        if self.tail:
-            self.tail.next = new_node
-            new_node.previous = self.tail
-        else:
-            self.head = new_node
-        self.tail = new_node
-        self.count += 1
+        """Adds an item to the back of the deque."""
+        self._list.append(item)
 
-    def prepend(self, item: T) -> None:
-        """Adds an item to the beginning of the list."""
-        new_node = self.Node(item)
-        if self.head:
-            self.head.previous = new_node
-            new_node.next = self.head
-        else:
-            self.tail = new_node
-        self.head = new_node
-        self.count += 1
+    def append_left(self, item: T) -> None:
+        """Adds an item to the front of the deque."""
+        self._list.prepend(item)
 
     def pop(self) -> T:
-        """Removes and returns the last item in the list."""
-        if not self.tail:
-            raise IndexError("List is empty")
-        value = self.tail.data
-        self.tail = self.tail.previous
-        if self.tail:
-            self.tail.next = None
-        else:
-            self.head = None
-        self.count -= 1
-        return value
+        """Removes and returns the last item in the deque."""
+        return self._list.pop()
 
-    def pop_front(self) -> T:
-        """Removes and returns the first item in the list."""
-        if not self.head:
-            raise IndexError("List is empty")
-        value = self.head.data
-        self.head = self.head.next
-        if self.head:
-            self.head.previous = None
-        else:
-            self.tail = None
-        self.count -= 1
-        return value
+    def pop_left(self) -> T:
+        """Removes and returns the first item in the deque."""
+        return self._list.pop_front()
 
     @property
     def front(self) -> T:
-        """Returns the first item in the list."""
-        if not self.head:
-            raise IndexError("List is empty")
-        return self.head.data
+        """Returns the first item in the deque."""
+        return self._list.front
 
     @property
     def back(self) -> T:
-        """Returns the last item in the list."""
-        if not self.tail:
-            raise IndexError("List is empty")
-        return self.tail.data
+        """Returns the last item in the deque."""
+        return self._list.back
 
     def __len__(self) -> int:
-        """Returns the number of items in the list."""
-        return self.count
+        """Returns the number of items in the deque."""
+        return len(self._list)
 
     def __contains__(self, item: T) -> bool:
-        """Checks if an item exists in the list."""
-        current = self.head
-        while current:
-            if current.data == item:
-                return True
-            current = current.next
-        return False
+        """Checks if an item exists in the deque."""
+        return item in self._list
 
     def clear(self) -> None:
-        """Clears all items from the list."""
-        self.head = None
-        self.tail = None
-        self.count = 0
+        """Clears all items from the deque."""
+        self._list.clear()
 
     def __iter__(self):
-        """Returns an iterator over the list."""
-        self._current = self.head
-        return self
-
-    def __next__(self) -> T:
-        """Returns the next item in the iteration."""
-        if self._current:
-            data = self._current.data
-            self._current = self._current.next
-            return data
-        else:
-            raise StopIteration
+        """Returns an iterator over the deque."""
+        return iter(self._list)
 
     def __str__(self) -> str:
-        """Returns a string representation of the list."""
-        values = []
-        current = self.head
-        while current:
-            values.append(repr(current.data))
-            current = current.next
-        return '[' + ' <-> '.join(values) + ']'
+        """Returns a string representation of the deque."""
+        return str(self._list)
+
+    def __repr__(self) -> str:
+        """Returns a detailed string representation of the deque."""
+        return f"Deque({repr(self._list)})"
